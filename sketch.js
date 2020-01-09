d5848b780ad64b174bd0cc14289b60489f664e86
Raul Cueto <56058579+ralfco23@users.noreply.github.com>
2020-01-09 11:30:45 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  createCanvas(1500, 700);
+
+}
+
+function draw() {
+  // put drawing code here
+  background(150, 0, 150)
+  fill(0, 250, 0)
+  ellipse(mouseX, mouseY, 400, 400)
+}
